818b6234fa6604e9dcaee29c17a3d3db5b715b45
Sopvia <112272251+Sopvia@users.noreply.github.com>
2024-07-05 15:56:00 +0200

    added english/german translation

---
+++ script.js
@@ -0,0 +1,39 @@
+let language = "english";
+function switchLang() {
+  if (language == "german") {
+    language = "english";
+  } else {
+    language = "german";
+  }
+
+  if (language == "german") {
+    document.getElementById("aboutMe").textContent = "Über mich";
+    document.getElementById("intro").textContent =
+      "Hallo, mein Name ist Sophia. Ich mache gerade eine Ausbildung zur Fachinformatikerin. Im Moment lerne ich Python, Java und GDscript.";
+    document.getElementById("github").textContent =
+      "Klicken Sie hier für mein Github-Profil.";
+    document.getElementById("skills").textContent = "Meine Fähigkeiten";
+    document.getElementById("projects").textContent = "Projekte";
+    document.getElementById("descSprite").textContent =
+      "Ein einfaches 2D-Jump 'n' Run-Spiel um Godot zu lernen.";
+    document.getElementById("descPY").textContent =
+      "Ein gewöhnlicher Python-Passwort-Generator (aber niedlich)";
+    document.getElementById("descRPS").textContent =
+      "Stein-Papier-Schere-Spiel";
+    document.getElementById("descSW").textContent = "Stoppuhr in JavaScript";
+  } else if (language == "english") {
+    document.getElementById("aboutMe").textContent = "About me";
+    document.getElementById("intro").textContent =
+      "Hi, my name is Sophia. I'm currently training to be an IT specialist. At the moment i'm learning Python, Java and GDscript.";
+    document.getElementById("github").textContent =
+      "Click here for my Github profile.";
+    document.getElementById("skills").textContent = "My Skills";
+    document.getElementById("projects").textContent = "Projects";
+    document.getElementById("descSprite").textContent =
+      "A simple 2D Jump 'n' Run game to explore Godot.";
+    document.getElementById("descPY").textContent =
+      "An ordinary Python Password Generator (but cute)";
+    document.getElementById("descRPS").textContent = "Rock Paper Scissors Game";
+    document.getElementById("descSW").textContent = "Stopwatch in JavaScript";
+  }
+}
